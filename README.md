dc4ccd3319fed75af8b0b31e23b4e0096c2b3c4d
Amber Garcia <amberg2212@gmail.com>
2025-11-23 14:57:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+**Dog Breed Information Application**
+
+
+**Description:**
+This project is a Flask-based web application that retrieves and displays information about dog breeds using data from The Dog API.
+Users can view breed characteristics, compare two breeds, and generate random dog images.
+
+
+**How to Run the Application:**
+1. Create and activate a virtual environment.
+
+
+2. Install the following Python packages:
+- Flask
+- Flask_WTF
+- WTForms
+- Requests
+- OS
+- main_functions
+3. Add a .env file containing the Dog API key (dog_key=your_key).
+
+
+4. Run the Flask application with python app.py.
+
+
+5. Open the application in a browser at http://127.0.0.1:5000.
+
+
+**API Used and Its Purpose**
+
+**The Dog API** is the primary data source for this web application.
+It provides information about various dog breeds including weight, height, origin, reason for breeding, temperament, and images.
+This application uses the API to populate the dropdown selection with all available breeds, retrieve detailed breed information,
+and provide a random image at the user's request.
+This API integration allows real-time data access, providing structured data presentation within the web app.
+
+**Features Implemented**
+
+- **Dropdown Menu:** allows users to select a dog breed and display its information.
+- **Text Input:** allows users to input a dog breed and display its information.
+- **Radio Button:** allows users to opt in to displaying a random dog image.
+- **Error and Missing-Data Handling:** manages invalid user input and missing API fields by substituting placeholder text (e.g., “Unknown”).
+- **Responsive User Interface:** designed with responsive to ensure readability across desktop and mobile devices.
---
+++ app.py
@@ -0,0 +1,115 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, RadioField, SubmitField, validators
+import os
+import main_functions
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+
+
+# function to get api key
+def get_key(filename,api_name):
+    api_key_dict = main_functions.read_from_file(filename)
+    my_api_key = api_key_dict[api_name]
+    return my_api_key
+
+
+dog_api_key = get_key("api_key.json","dog_key")
+
+
+# function to save api data to a json
+def save_breeds(dog_api_key):
+    url = "https://api.thedogapi.com/v1/breeds"
+    headers = {"x-api-key": dog_api_key}
+    breed_dict = requests.get(url, headers=headers).json()
+    main_functions.save_to_file(breed_dict,"breeds.json")
+
+# save json to local computer
+# save_breeds(dog_api_key)
+
+
+# load all breeds - for dropdown section in form
+def get_breeds():
+    return main_functions.read_from_file("breeds.json")
+
+
+# get breed information - for dropdown section in form
+def get_breed_info(breed_name):
+    for breed in get_breeds():
+        if breed["name"].lower() == breed_name.lower():
+
+            # combine imperial and metric formats into one line
+            weight_combined = f'{breed["weight"]["imperial"]} lbs / {breed["weight"]["metric"]} kg'
+            height_combined = f'{breed["height"]["imperial"]} in / {breed["height"]["metric"]} cm'
+
+            return {
+                "name": breed["name"],
+                "weight": weight_combined,
+                "height": height_combined,
+                "lifespan": breed.get("life_span", "Unknown"),
+                "origin": breed.get("origin", "Unknown"),
+                "bred for": breed.get("bred_for", "Unknown"),
+                "temperament": breed.get("temperament", "Unknown"),
+                "image": breed.get("image", {}).get("url")
+            }
+    return None
+
+
+# function to get random dog image - for radio button section in form
+def get_random_image():
+    url = "https://api.thedogapi.com/v1/images/search?limit=1"
+    headers = {"x-api-key": dog_api_key}
+    data = requests.get(url, headers=headers).json()
+    if not data:
+        return None
+    return data[0]["url"]
+
+
+# creating the form
+class UserForm(FlaskForm):
+    # dropdown for user to choose dog breed #1
+    breed_dropdown = SelectField("Select a dog breed to see its information",
+                                 [validators.DataRequired()], choices=[])
+
+    # text input for user to enter dog breed #2
+    breed_input = StringField("Enter a dog breed to compare information", [validators.DataRequired()])
+
+    # radio button (y/n) for random dog image
+    random_pic = RadioField("Do you want to see a random dog picture?",
+                            choices=[("Yes", "Yes"), ("No", "No")], default="No")
+
+    # submit
+    submit = SubmitField("Submit")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = UserForm()
+
+    # populate dropdown based on breeds in json
+    breeds = get_breeds()
+    form.breed_dropdown.choices = [(item["name"], item["name"]) for item in breeds]
+
+    if request.method == "POST":
+        breed_dropdown_entered = form.breed_dropdown.data
+        breed_input_entered = form.breed_input.data
+        random_pic_entered = form.random_pic.data
+
+        # breed information - dropdown form section
+        breed1_info = get_breed_info(breed_dropdown_entered)
+
+        # breed information - text input section
+        breed2_info = get_breed_info(breed_input_entered)
+
+        # random image - radio button form section
+        random_image = get_random_image() if random_pic_entered == "Yes" else None
+
+        return render_template("form_results.html", breed1_info=breed1_info, breed2_info=breed2_info,
+                               random_image=random_image)
+
+    return render_template('form.html', form=form)
+
+if __name__ == '__main__':
+    app.run()
